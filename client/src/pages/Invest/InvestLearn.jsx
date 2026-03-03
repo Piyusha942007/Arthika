8a817185b8665540d9f9080b350f653ad3475a9d
theanvisharma <theanvisharma@gmail.com>
2026-03-03 14:46:42 +0530

    Added Clickable links to InvestLearn Page

--- client/src/pages/Invest/InvestLearn.jsx
+++ client/src/pages/Invest/InvestLearn.jsx
@@ -20,22 +20,24 @@ export default function InvestLearn() {
 
         {/* TOP ROW: GOVT SCHEMES */}
         <div className="schemes-row">
-          <div className="scheme-box">
+          <a href="https://www.pmindia.gov.in/hi/government_tr_rec/%E0%A4%AC%E0%A5%87%E0%A4%9F%E0%A5%80-%E0%A4%AC%E0%A4%9A%E0%A4%BE%E0%A4%93-%E0%A4%AC%E0%A5%87%E0%A4%9F%E0%A5%80-%E0%A4%AA%E0%A4%A2%E0%A4%BC%E0%A4%BE%E0%A4%93-%E0%A4%AC%E0%A4%BE%E0%A4%B2/" target="_blank" rel="noreferrer" className="scheme-box">
             <img src={beti} alt="Beti Bachao" className="scheme-img" />
-          </div>
-          <div className="scheme-box">
+          </a>
+
+          <a href="https://www.pib.gov.in/PressReleasePage.aspx?PRID=1742800&reg=3&lang=2" target="_blank" rel="noreferrer" className="scheme-box">
             <img src={rastriya} alt="Rastriya Mahila" className="scheme-img" />
-          </div>
-          <div className="scheme-box">
+          </a>
+
+          <a href="https://wcd.delhi.gov.in/wcd/pradhan-mantri-matru-vandana-yojana-pmmvy" target="_blank" rel="noreferrer" className="scheme-box">
             <img src={pradhan} alt="Pradhan Mantri" className="scheme-img" />
-          </div>
+          </a>
         </div>
 
         {/* BANK SECTIONS */}
         <div className="bank-sections-list">
           
           {/* HDFC SECTION */}
-          <div className="bank-card-long">
+          <a href="https://www.hdfc.bank.in/savings-account/women-savings-account" target="_blank" rel="noreferrer" className="bank-card-long">
             <ul className="benefit-bullets">
               <li>Cash allowance for every day of hospitalization due to an accident.</li>
               <li>Lower interest rates on two-wheeler and auto loans.</li>
@@ -43,10 +45,10 @@ export default function InvestLearn() {
             <div className="bank-logo-container">
               <img src={hdfc} alt="HDFC Bank" className="bank-brand-img" />
             </div>
-          </div>
+          </a>
 
           {/* SBI SECTION (REVERSED LAYOUT) */}
-          <div className="bank-card-long reversed">
+          <a href="https://sbi.bank.in/web/business/sme/sme-loans/sme-loan-to-women-entrepreneur-sbi-asmita" target="_blank" rel="noreferrer" className="bank-card-long reversed">
             <div className="bank-logo-container">
               <img src={sbi} alt="SBI" className="bank-brand-img" />
             </div>
@@ -54,10 +56,10 @@ export default function InvestLearn() {
               <li>Lowered interest rates (0.5% concession) for businesses where women own more than 50%.</li>
               <li>No security/collateral required for loans up to ₹5 Lakh</li>
             </ul>
-          </div>
+          </a>
 
           {/* BANK OF BARODA SECTION */}
-          <div className="bank-card-long">
+          <a href="https://bankofbaroda.bank.in/accounts/saving-accounts/bob-mahila-shakti-saving-account" target="_blank" rel="noreferrer" className="bank-card-long">
             <ul className="benefit-bullets">
               <li>Collateral-free loans up to ₹5 Lakh for women entrepreneurs.</li>
               <li>No processing fees for loans up to ₹6 Lakh for Self-Help Groups (SHGs)</li>
@@ -65,7 +67,7 @@ export default function InvestLearn() {
             <div className="bank-logo-container">
               <img src={bank} alt="Bank of Baroda" className="bank-brand-img" />
             </div>
-          </div>
+          </a>
 
         </div>
       </main>
